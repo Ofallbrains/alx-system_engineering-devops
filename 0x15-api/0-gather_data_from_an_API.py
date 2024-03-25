1285327885c9860720cf81c36e01abda1da245c9
Ofallbrains <ofallbrains@gmail.com>
2024-03-25 16:29:10 +0000

    API project

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,25 +1,14 @@
 #!/usr/bin/python3
-"""
-Queries information from the JSON placeholder API.
-Returns the information about an employee's TODO list progress.
+"""Returns to-do list information for a given employee ID."""
 import requests
 import sys
 
 if __name__ == "__main__":
-        url = 'https://jsonplaceholder.typicode.com/'
-            user_url = url + "users/{}".format(sys.argv[1])
-                todo_url = url + "todos"
-                    params = {"userId": sys.argv[1]}
-                        user = requests.get(url=user_url).json()
-                            todos = requests.get(url=todo_url, params=params).json()
-                                completed = []
+        url = "https://jsonplaceholder.typicode.com/"
+            user = requests.get(url + "users/{}".format(sys.argv[1])).json()
+                todos = requests.get(url + "todos", params={"userId": sys.argv[1]}).json()
 
-                                    for todo in todos:
-                                            if todo.get("completed") is True:
-                                                        completed.append(todo.get("title"))
-
-                                                            print("Employee {} is done with tasks({}/{}):".
-                                                                      format(user.get("name"), len(completed), len(todos)))
-
-                                                                          for complete in completed:
-                                                                                  print("\t {}".format(complete))
+                    completed = [t.get("title") for t in todos if t.get("completed") is True]
+                        print("Employee {} is done with tasks({}/{}):".format(
+                                    user.get("name"), len(completed), len(todos)))
+                            [print("\t {}".format(c)) for c in completed]
